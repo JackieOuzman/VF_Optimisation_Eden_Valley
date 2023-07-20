61f2e0d12f00683971b3824d21e62c2e2f2075a7
JackieOuzman <jackie.ouzman@csiro.au>
2023-07-20 15:17:38 +0930

    I don use this all done in step 1

--- step 1b_VF_animal_logs.R
+++ step 1b_VF_animal_logs.R
@@ -10,15 +10,34 @@ library(sf)
 
 
 ############################################################################################
+############                  bring in boundaries             ##############################
+############################################################################################
+
+Hard_fence_bound <- st_read("W:/VF/Optimising_VF/raw_data/Eden Valley/VF_Boundary/EdenValley_site1GDA_a.shp")  # this is the hard fences
+Hard_fence_bound <-
+  st_transform(Hard_fence_bound, crs = 28354)
+
+# Chiswick_hard_fence_bound_buff <- st_read("W:/VF/Sheep_Chiswick_2022/spatial_boundaries/Chiswick_paddock_boundary_final_buff10.shp")  # this is the 
+# Chiswick_hard_fence_bound_buff <-
+#   st_transform(Chiswick_hard_fence_bound_buff, crs = 28354)
+
+
+VF1_paddock <-   st_read("W:/VF/Optimising_VF/raw_data/Eden Valley/VF_Boundary/VF1_graze.shp")
+# VF_paddock <-  st_transform(VF_paddock, crs = 28354)
+
+
+
+
+############################################################################################
 ############       bring in data created in step1a           ##############################
 ############################################################################################
 
-path_step1 <- "W:/VF/Optimising_VF/Chiswick/data_prep/"
-raw_data <- "W:/VF/Optimising_VF/raw_data/Chiswick/"
+path_step1 <- "W:/VF/Optimising_VF/Eden Valley/data_prep/"
+raw_data <- "W:/VF/Optimising_VF/Eden Valley/data_prep/step1"
 
-animal_GPS_data_1 <- read_csv(paste0(raw_data, "db_trial_csiro_armidale_chiswick_mob_259_filtered.csv"))
-animal_GPS_data_2 <- read_csv(paste0(raw_data, "db_trial_csiro_armidale_chiswick_neckband_serial_9380142_filtered.csv"))
-GPS <- rbind(animal_GPS_data_1, animal_GPS_data_2)
+
+GPS <- readRDS("W:/VF/Optimising_VF/Eden Valley/data_prep/step1/VF1_InclusionBord.rds")
+
 
 
 str(GPS)
@@ -26,18 +45,19 @@ str(GPS)
 
 #format time and date clm from character to time
 GPS <-  GPS %>%
-  mutate(timeOfEvent = as.POSIXct(timeOfEvent, tz = "GMT", format = "%d/%m/%Y %H:%M"))
+  mutate(timeOfEvent = as.POSIXct(time, tz = "GMT", format = "%d/%m/%Y %H:%M"))
 
 
 GPS <- GPS %>% 
-  mutate(GMT = ymd_hms(timeOfEvent, tz = "GMT"))
+  mutate(GMT = ymd_hms(time, tz = "GMT"))
 
 GPS <- GPS %>% 
-  mutate(local_time = with_tz(GMT, tz = "Australia/Sydney"))
+  mutate(local_time = with_tz(GMT, tz = "Australia/Adelaide"))
 
 ## Add a clm for ID_jaxs
 GPS <- GPS %>% 
-  dplyr::mutate( ID_jaxs = row_number())
+  dplyr::mutate( ID_jaxs = row_number()) %>% 
+  dplyr::mutate(fencesID = "VF1")
 
 
 ### what are the fences called in this dataset?
@@ -45,45 +65,24 @@ unique(GPS$fencesID) # we only have 3 "NULL"  "1dd82" "1f1eb" "1766d"
 
 
 ## reorder the clms
-GPS <- GPS %>% 
-  dplyr::select(ID_jaxs,deviceUIDHex:local_time)
 
 
 
 GPS <- GPS %>% 
-  mutate(date = as.Date(local_time, tz= "Australia/Sydney"),
+  mutate(date = as.Date(local_time, tz= "Australia/Adelaide"),
          DOY = yday(date))
 
 
 #############################################################################################
-####    Assign collar to sheep names #####
-unique(GPS$deviceName)
-
-GPS <- GPS %>% 
-  mutate(Sheep_ID = case_when(
-    deviceName == 9380142  ~ "1",
-    deviceName == 9380674  ~ "2",
-    deviceName == 9380743  ~ "3",
-    deviceName == 9380451  ~ "4",
-    deviceName == 9380265  ~ "5",
-    deviceName == 9380470  ~ "6",
-    TRUE                      ~ "other"
-    
-    
-  ))
-#only keep the collar that sue said:)
+####    Assign collar to animal names - I did this in step 1#####
+u
+#only keep records with animal ID
 GPS <- GPS %>%
-  filter(Sheep_ID != "other")
+  filter(animal_ID != "other")
 
 ## ok lets just remove the Nulls
 GPS <- GPS %>% 
   filter(fencesID!= "NULL")
-
-## reorder the clms
-# GPS <- GPS %>% 
-#   dplyr::select(ID_jaxs,Sheep_ID, deviceUIDHex:local_time)
-
-
 
 
 str(GPS)
@@ -103,12 +102,12 @@ GPS <- GPS %>%
 #turn into spatial data
 GPS_sf <-
   st_as_sf(GPS,
-           coords = c("gpsData.lng", "gpsData.lat"),
+           coords = c("lon", "lat"),
            crs = 4326,
            agr = "constant")
 
 GPS_sf_trans <-
-  st_transform(GPS_sf, crs = 28355)
+  st_transform(GPS_sf, crs = 28354)
 
 
 rm(GPS_sf)
@@ -116,29 +115,13 @@ rm(GPS_sf)
 
 str(GPS_sf_trans)
 
-############################################################################################
-############                  bring in boundaries             ##############################
-############################################################################################
 
-Chiswick_hard_fence_bound <- st_read("W:/VF/Sheep_Chiswick_2022/spatial_boundaries/Chiswick_paddock_boundary_final.shp")  # this is the hard fences
-Chiswick_hard_fence_bound <-
-  st_transform(Chiswick_hard_fence_bound, crs = 28355)
-
-Chiswick_hard_fence_bound_buff <- st_read("W:/VF/Sheep_Chiswick_2022/spatial_boundaries/Chiswick_paddock_boundary_final_buff10.shp")  # this is the 
-Chiswick_hard_fence_bound_buff <-
-  st_transform(Chiswick_hard_fence_bound_buff, crs = 28355)
-
-
-VF_paddock <-   st_read("W:/VF/Sheep_Chiswick_2022/spatial_boundaries/VF_paddock.shp")
-VF_paddock <-  st_transform(VF_paddock, crs = 28355)
-
-water_pt <-  st_read("W:/VF/Sheep_Chiswick_2022/spatial_boundaries/water_pt.shp")
 
 
 
 ggplot() +
-  geom_sf(data = Chiswick_hard_fence_bound, color = "black", fill = NA) +
-  geom_sf(data = VF_paddock, color = "black", fill = NA) +
+  geom_sf(data = Hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = VF1_paddock, color = "black", fill = NA) +
   geom_sf(data = GPS_sf_trans ,alpha = 0.03) +
   theme_bw()+
   facet_wrap(.~ date)+
@@ -148,156 +131,9 @@ ggplot() +
 
 
 ################################################################################
-#### filtering out GPS data based on times start and end of the trial
-
-GPS_sf_trans <- GPS_sf_trans %>% 
-  filter(
-    local_time >=  ymd_hms("2022-06-28 09:50:00", tz= "Australia/Sydney"))
-
-GPS_sf_trans <- GPS_sf_trans %>% 
-  filter(
-    local_time <=  ymd_hms("2022-07-02 10:10:00", tz= "Australia/Sydney"))
+#### filtering out GPS data based on times start and end of the trial # I have done this already###
 
 
-### define a training period with new clm No training period aniamls were allowed to acclimatise in neighboring paddock
-
-# GPS_sf_trans <- GPS_sf_trans %>% 
-#   mutate(training_period = case_when(
-#     local_time <= ymd_hms("2022-10-17 13:10:00", tz= "Australia/Sydney")~ "training",
-#     TRUE                      ~ "non_training"
-#     
-#   ))
-
-
-# Times sheep were brought in each day for the VF Chiswick trial;
-# 28/6- sheep out 9:50
-# 29/6 11:21- 12:21
-# 30/6 10:34- 11:36
-# 1/7- 10:37- 11:20
-# 2/7- Brought in at 10:10
-#### each day the animals were yarded so i need to remove this data
-
-# let divide the data per day
-day_28 <- GPS_sf_trans %>%  filter(date == "2022-06-28")
-day_29 <- GPS_sf_trans %>%  filter(date == "2022-06-29")
-day_30 <- GPS_sf_trans %>%  filter(date == "2022-06-30")
-day_1 <- GPS_sf_trans %>%  filter(date == "2022-07-01")
-day_2 <- GPS_sf_trans %>%  filter(date == "2022-07-02")
-
-
-# keep everything after before yarding and after yarding
-
-day_29_before_yarding <- day_29 %>%
-  filter(local_time <=  ymd_hms("2022-06-29 11:21:00", tz = "Australia/Sydney"))
-day_29_after_yarding <- day_29 %>%
-  filter(local_time >=  ymd_hms("2022-06-29 12:21:00", tz = "Australia/Sydney"))
-
-day_29_clean <- rbind(day_29_before_yarding, day_29_after_yarding)
-rm(day_29_before_yarding, day_29_after_yarding, day_29)
-
-
-day_30_before_yarding <- day_30 %>%
-  filter(local_time <=  ymd_hms("2022-06-30 10:34:00", tz = "Australia/Sydney"))
-day_30_after_yarding <- day_30 %>%
-  filter(local_time >=  ymd_hms("2022-06-30 11:36:00", tz = "Australia/Sydney"))
-
-day_30_clean <- rbind(day_30_before_yarding, day_30_after_yarding)
-rm(day_30_before_yarding, day_30_after_yarding, day_30)
-
-day_1_before_yarding <- day_1 %>%
-  filter(local_time <=  ymd_hms("2022-07-01 10:37:00", tz = "Australia/Sydney"))
-day_1_after_yarding <- day_1 %>%
-  filter(local_time >=  ymd_hms("2022-07-01 11:20:00", tz = "Australia/Sydney"))
-
-day_1_clean <- rbind(day_1_before_yarding, day_1_after_yarding)
-rm(day_1_before_yarding, day_1_after_yarding, day_1)
-
-
-### put it back togther 
-
-animals_GPS_trim_time <- rbind(day_28, day_29_clean, day_30_clean, day_1_clean, day_2)
-
-rm(day_28, day_29_clean, day_30_clean, day_1_clean, day_2)
-
-########################################################################################
-
-
-
-########################################################################################
-
-### remove the water and other animals logs
-
-unique(animals_GPS_trim_time$Sheep_ID)
-
-animals_GPS_trim_time <- animals_GPS_trim_time %>% 
-  filter(Sheep_ID !=  "other") %>% 
-  filter(Sheep_ID !=  "water_pt")
-
-
-
-
-## check
-
-ggplot() +
-  geom_sf(data = Chiswick_hard_fence_bound, color = "black", fill = NA) +
-  geom_sf(data = VF_paddock, color = "black", fill = NA) +
-  geom_sf(data = animals_GPS_trim_time ,alpha = 0.03) +
-  facet_wrap(.~ date)+
-  theme_bw()+
-  theme(legend.position = "none",
-        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
-  labs(title = "All animal logs trimmed time",
-       subtitle = "log when animals were yarded removed")
-
-
-
-# -------------------------------------------------------------------------------------------------- ###
-
-
-#I think this should be the end of step 1.
-
-
-#Next steps boundaries trim location and time
-## merge in other animal data
-## look for weather data ? anything else??
-
-
-
-
-########################################################################################################
-
-
-
-output_path <- "W:/VF/Optimising_VF/Chiswick/data_prep"  #animals_GPS_trim_time
-
-
-############################################################################################################################
-### format the aniaml log data so I output the clm with local time and keep time difference cals and have clm for x and y
-
-## convert the geom clm into x and y clms
-str(animals_GPS_trim_time)
-
-coordinates <-as.data.frame( st_coordinates(animals_GPS_trim_time))
-GPS_trim_time_df <- as.data.frame(animals_GPS_trim_time)
-
-GPS_trim_time_df <- GPS_trim_time_df %>% 
-  dplyr::select(-"geometry")
-
-
-GPS_trim_time <-   cbind(GPS_trim_time_df,coordinates )
-## ensure the date and time clms are outputting and outputting in the correct format.
-
-
-GPS_trim_time$local_time <-   format(GPS_trim_time$local_time, usetz=TRUE)
-GPS_trim_time$GMT        <-   format(GPS_trim_time$GMT, usetz=TRUE)
-GPS_trim_time$start_fence <-  format(GPS_trim_time$start_fence, usetz=TRUE)
-GPS_trim_time$end_fence    <- format(GPS_trim_time$end_fence, usetz=TRUE)
-GPS_trim_time$start_trial    <- format(GPS_trim_time$start_trial, usetz=TRUE)
-
-write.csv(GPS_trim_time, 
-          paste0(output_path,"/Step1b_animals_GPS_trim_time.csv"), 
-          row.names=FALSE)
-#############################################################
 
 
 
